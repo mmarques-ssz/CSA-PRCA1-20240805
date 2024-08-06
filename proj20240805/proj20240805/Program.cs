f51da0c2aa064fef31f049d2ed1173fe9ac3345f
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2024-08-05 22:25:32 -0300

    Add files via upload

---
+++ proj20240805/proj20240805/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj20240805
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double media;
+            int m;
+            int[] n;
+            n = new int[100];
+            int ac;
+            Random r;
+            r = new Random();
+
+            
+            ac = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                n[i] = r.Next(98) + 2;
+                ac += n[i];
+                Console.WriteLine(n[i]);
+            }
+            Console.WriteLine(ac);
+            media = (double)ac / 100;
+            Console.WriteLine(media);
+
+            m = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                if (n[i]>media)
+                {
+                    m++;
+                }
+            }
+            Console.WriteLine(m);
+
+        }
+    }
+}
